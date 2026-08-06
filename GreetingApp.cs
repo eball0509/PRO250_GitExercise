9d322a2c52466f5ada52dceb7788b08aa305dc8b
eball0509 <161506233+eball0509@users.noreply.github.com>
2026-08-05 21:10:49 -0600

    (This is a personal fix, i wanted this here)

--- GreetingApp.cs
+++ GreetingApp.cs
@@ -1,29 +1,40 @@
-// How would you like to be greeted
+using System;
 
-int choice;
-
-string choiceAsString;
-
-Console.WriteLine("How would you like to be greeted?");
-
-Console.WriteLine("1. Non Formal");
-Console.WriteLine("2. Formal");
-
-choiceAsString = Console.ReadLine();
-
-choice = int.Parse(choiceAsString);
-
-switch (choice)
+internal static class GreetingApp
 {
-    case 1:
-        Console.WriteLine("Hey there! How's it going?");
-        break;
+    private static void Main()
+    {
+        int choice;
 
-    case 2:
-        Console.WriteLine("Good day to you! How do you do?");
-        break;
+        string? choiceAsString;
 
-    default:
-        Console.WriteLine("Invalid choice. Please select either 1 or 2.");
-        break;
+        while (true)
+        {
+            Console.WriteLine("How would you like to be greeted?");
+
+            Console.WriteLine("1. Non Formal");
+            Console.WriteLine("2. Formal");
+
+            choiceAsString = Console.ReadLine();
+
+            choice = int.Parse(choiceAsString);
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Hey there! How's it going?");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Good day to you! How do you do?");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please select either 1 or 2.");
+                    break;
+            }
+
+            break;
+        }
+    }
 }
